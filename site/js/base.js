fc592dc6d8f97d5b1c062608d4773654e5e5fb3f
Bob Tiernay <btiernay@oicr.on.ca>
2015-12-17 20:08:50 -0500

    Cleanup.

--- site/js/base.js
+++ site/js/base.js
@@ -1,7 +1,8 @@
 $(function () {
-   var local = window.location.toString();
+   var local = document.URL;
    var base = 'http://www.zerohedge.com';
-   load(base, false);
+   var url = base + window.location.hash.replace("#","");
+   load(url, false);
 
    var $content = $("#content");
 
@@ -13,7 +14,7 @@ $(function () {
    function load(url, scroll) {
       $.get("https://crossorigin.me/" + url, function (data) {
          var $html = $(data);
-         $html.find(".links,script,.js-links,.similar-box,.content-box-1 > .picture,.clear-block").remove();
+         $html.find("h1:empty, .links,script,.js-links,.similar-box,.content-box-1 > .picture, .node > .picture, .tabs").remove();
          $html.find(".node .submitted").nextUntil(".content").remove();
          $html.find("img").each(function () {
             var src = $(this).attr("src");
@@ -25,13 +26,30 @@ $(function () {
             var zh = href.indexOf("www.zerohedge.com") > 0;
 
             if (zh || href.indexOf("http://") < 0) {
-               history.pushState({}, '', href.replace(base, ""));
+               history.pushState({}, '', "#" + href.replace(base, ""));
             }
 
             href = zh ? href : "http://www.zerohedge.com" + href;
             href = href.replace("/articles", "/");
             load(href, true);
             return false;
+         });
+         $html.find(".submitted").each(function(){
+            var $date = $(this);
+            var text = $(this).text();
+            var date = text.replace("Submitted by Tyler Durden on ","");
+            var d = parseDate(date);
+            if (isToday(d)) {
+               var hr = d.getHours();
+               var min = d.getMinutes();
+               if (min < 10) {
+                   min = "0" + min;
+               }
+               var ampm = hr < 12 ? "am" : "pm";
+               $date.text((hr <= 12 ? hr : hr - 12) + ":" + min + " " + ampm);
+            } else {
+               $date.text(date);
+            }
          });
 
          // Show
@@ -42,4 +60,16 @@ $(function () {
          }
       });
    }
+
+   function parseDate(s){
+      var re = /.*(\d\d)\/(\d\d)\/(\d{4}) (?:- )?(\d\d):(\d\d).*/;
+      var m = re.exec(s);
+      return m ? new Date(m[3], m[1]-1, m[2], m[4], m[5]) : null;
+   }
+
+   function isToday(td){
+      var d = new Date();
+      return td.getDate() == d.getDate() && td.getMonth() == d.getMonth() && td.getFullYear() == d.getFullYear();
+   }
+
 });
